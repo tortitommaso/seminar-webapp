63049686b485acae148452f7c5957c1d6a83d09e
ttorti <torti.tommaso@gmail.com>
2017-03-01 16:02:32 +0100

    first test

---
+++ src/test/java/com/RoutesTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoutesTest extends Routes {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
