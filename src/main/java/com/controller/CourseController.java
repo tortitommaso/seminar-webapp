7cc792a5a47dbcac4b7b7e29e504e95df397eca6
ttorti <torti.tommaso@gmail.com>
2017-03-09 08:39:23 +0100

    extract methods

--- src/main/java/com/controller/CourseController.java
+++ src/main/java/com/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,18 +16,29 @@ public class CourseController implements Controller {
 	public void create(HttpServletRequest req, HttpServletResponse resp, RequestMethod requestMethod) throws Exception  {
 		CourseRepository courseRepository = new CourseRepository();
 		if(requestMethod.equals(RequestMethod.GET)) {
-			List<Course> courses = courseRepository.all();
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.getWriter().println(new CourseView().creationForm(courses));
+			showCourses(resp, courseRepository);
 		} 
 		if(requestMethod.equals(RequestMethod.POST)) {
-			String courseName = req.getParameter("courseName");
-			int totalSeats = Integer.parseInt(req.getParameter("totalSeats"));
-			Course course = new Course(courseName, totalSeats);
-			courseRepository.add(course);
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.sendRedirect("/course/create");								
+			createCourse(req, resp, courseRepository);								
 		}
+	}
+
+	private void createCourse(HttpServletRequest req, HttpServletResponse resp, CourseRepository courseRepository)
+			throws IOException {
+		String courseName = req.getParameter("courseName");
+		int totalSeats = Integer.parseInt(req.getParameter("totalSeats"));
+		Course course = new Course(courseName, totalSeats);
+		
+		courseRepository.add(course);
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.sendRedirect("/course/create");
+	}
+
+	private void showCourses(HttpServletResponse resp, CourseRepository courseRepository)
+			throws IOException, Exception {
+		List<Course> courses = courseRepository.all();
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.getWriter().println(new CourseView().creationForm(courses));
 	}
 
 }
